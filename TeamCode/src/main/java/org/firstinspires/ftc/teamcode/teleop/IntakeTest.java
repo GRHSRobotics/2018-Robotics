efa2dc4f42444bcd65e8c46ed18744eb275dd4cb
colinblake20 <cblake149@gmail.com>
2019-03-05 15:17:39 -0500

    inproved intakeTest class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/IntakeTest.java
@@ -2,11 +2,20 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.HardwareDefinitions;
 
 @TeleOp(name = "IntakeTest", group = "Test")
 public class IntakeTest extends HardwareDefinitions {
+
+    boolean useHingeEncoders = false;
+
+    public int hingeCountsPerRotation = 288;
+    public int hingeIncrement = 1;
+    public int hingeInitialPosition;
+    public int hingeMaxDelta = hingeCountsPerRotation / 4;
+    public int hingeExtendedPosition;
 
     @Override
     public void runOpMode(){
@@ -15,6 +24,9 @@ public class IntakeTest extends HardwareDefinitions {
         telemetry.addData("Robot is initialized", "");
 
         waitForStart();
+        hingeInitialPosition = intakeHinge.getCurrentPosition();
+        hingeExtendedPosition = hingeInitialPosition + hingeMaxDelta;
+
         telemetry.addData("Robot is started", "" );
 
 
@@ -30,25 +42,52 @@ public class IntakeTest extends HardwareDefinitions {
             }
 
             if(gamepad1.a){
+                useHingeEncoders = true;
+                telemetry.addData("useHingeEncoders: ", "true");
             }
 
             if(gamepad1.b){
+                useHingeEncoders = false;
+                telemetry.addData("useHingeEncoders: ", "false");
             }
 
             if(gamepad1.right_trigger > 0){
                 intakeActuator.setPower(1);
+
             } else if(gamepad1.right_bumper){
                 intakeActuator.setPower(-1);
             } else{
                 intakeActuator.setPower(0);
             }
 
-            if(gamepad1.left_trigger > 0){
-                intakeHinge.setPower(1);
-            } else if (gamepad1.left_bumper){
-                intakeHinge.setPower(-1);
-            } else{
-                intakeHinge.setPower(0);
+            if(useHingeEncoders) {
+                intakeHinge.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                if (gamepad1.left_trigger > 0) {
+                    if (intakeHinge.getCurrentPosition() < hingeExtendedPosition) {
+                        intakeHinge.setTargetPosition(intakeHinge.getCurrentPosition() + hingeIncrement);
+                        intakeHinge.setPower(1);
+                    }
+                } else if (gamepad1.left_bumper) {
+                    if (intakeHinge.getCurrentPosition() > hingeInitialPosition) {
+                        intakeHinge.setTargetPosition(intakeHinge.getCurrentPosition() - hingeIncrement);
+                        intakeHinge.setPower(1);
+                    }
+                } else {
+                    if (intakeHinge.isBusy()) {
+                        intakeHinge.setPower(1);
+                    } else {
+                        intakeHinge.setPower(0);
+                    }
+                }
+            } else {
+                intakeHinge.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                if(gamepad1.left_trigger > 1){
+                    intakeHinge.setPower(1);
+                } else if (gamepad1.left_bumper){
+                    intakeHinge.setPower(-1);
+                } else{
+                    intakeHinge.setPower(0);
+                }
             }
 
             if(gamepad1.dpad_down){
